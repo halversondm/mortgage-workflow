f107b549e80c34b60e15fd786e0d186db7a27b59
Dan Halverson <daniel.m.halverson@gmail.com>
2018-04-05 07:39:39 -0500

    initial commit

---
+++ src/main/java/com/halversondm/mortgage/MortgageWorkflowApplication.java
@@ -0,0 +1,12 @@
+package com.halversondm.mortgage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageWorkflowApplication.class, args);
+	}
+}
